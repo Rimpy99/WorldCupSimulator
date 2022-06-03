003e110ff34804c46066c5a07aa4110bd745ddf1
Rimpy99 <70006pawel@gmail.com>
2022-06-03 16:46:02 +0200

    Updates

---
+++ precache-manifest.d4863049f12270e9195759ee14b1f417.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db522ba2450f7f25324",
+    "url": "/WorldCupSimulator/static/css/main.fd3bffee.chunk.css"
+  },
+  {
+    "revision": "1db522ba2450f7f25324",
+    "url": "/WorldCupSimulator/static/js/main.1db522ba.chunk.js"
+  },
+  {
+    "revision": "78d0354b2f63aeb6d40d",
+    "url": "/WorldCupSimulator/static/js/1.78d0354b.chunk.js"
+  },
+  {
+    "revision": "6dbc4baf6e3d106d7758",
+    "url": "/WorldCupSimulator/static/js/runtime~main.6dbc4baf.js"
+  },
+  {
+    "revision": "5662577c359027635fe2f95a28864aa5",
+    "url": "/WorldCupSimulator/static/media/stadium.5662577c.jpg"
+  },
+  {
+    "revision": "4117758245aad6320dee2f53806f60de",
+    "url": "/WorldCupSimulator/static/media/ecuador.41177582.png"
+  },
+  {
+    "revision": "3bd7a4bd17f53fcd16406bd27ea8ff97",
+    "url": "/WorldCupSimulator/static/media/portugal.3bd7a4bd.png"
+  },
+  {
+    "revision": "1102b4d4bc31c1ab334f04d0ce7827b7",
+    "url": "/WorldCupSimulator/static/media/serbia.1102b4d4.png"
+  },
+  {
+    "revision": "33892f3673049283f1619fe9e6b7f969",
+    "url": "/WorldCupSimulator/index.html"
+  }
+];
